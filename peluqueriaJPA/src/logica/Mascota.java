1cb787691e5e351648fff9d51e0799b90674c0fb
diego vilca <vicv87.mp@gmail.com>
2021-11-16 17:12:26 -0300

    actualizo a @generatedValue(strategy = GenerationType.IDENTITY)

--- peluqueriaJPA/src/logica/Mascota.java
+++ peluqueriaJPA/src/logica/Mascota.java
@@ -12,7 +12,7 @@ import javax.persistence.Id;
 public class Mascota implements Serializable {
     //atributos
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     @Basic
     private String num_cliente;
